4b1c134b0e4728273145c5d85fb3218975f8a2ca
Nachat Ayoub <73206390+nachat-ayoub@users.noreply.github.com>
2024-05-05 19:46:45 +0100

    Update decode.js

--- decode.js
+++ decode.js
@@ -1,4 +1,6 @@
+window.onload = function() {
 
+    
 /**
  * Decodes an obfuscated string back to a JSON object.
  * @param {string} obfuscatedString - The obfuscated string to be decoded.
@@ -29,3 +31,37 @@ function decodeJSON(obfuscatedString) {
   console.log(jsonObject)
   return jsonObject;
 }
+  
+    
+  // Function to parse URL query parameters
+  function getQueryParam(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
+  }
+
+  // Check if the &quot;v&quot; query parameter exists
+  const videoParam = getQueryParam(&quot;v&quot;);
+  if (videoParam) {
+    
+
+	const data = decodeJSON(videoParam)
+    console.log({data})
+    if(data &amp;&amp; data?.w &amp;&amp; data?.p &amp;&amp; data?.b &amp;&amp; data?.n) {
+
+    // Get the element with ID &quot;watch-container&quot;
+    const watchContainer = document.getElementById(&quot;watch-container&quot;);
+    // If the element exists, set its innerHTML to the specified iframe code
+    if (watchContainer) {
+      watchContainer.innerHTML = `
+        <iframe class='w-100' src='${data.w}' style='aspect-ratio:16/9;'/>
+
+        <div class='d-flex justify-content-between mt-3'>
+          <a class='btn btn-primary' href='${data.p}'>Prev</a>
+          <a class='btn btn-secondary' href='${data.b}'>Back</a>
+          <a class='btn btn-success' href='${data.n}'>Next</a>
+        </div>
+    `;
+    }
+    }
+  }
+};
