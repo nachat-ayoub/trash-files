1668243086345a0c576c9a8da851fcca22104561
Nachat Ayoub <73206390+nachat-ayoub@users.noreply.github.com>
2024-05-14 00:54:05 +0100

    Update blogger-embed.js

--- blogger-embed.js
+++ blogger-embed.js
@@ -53,7 +53,7 @@ $(document).ready(function () {
 
     if (data && data?.s && data?.p && data?.b && data?.n && data?.title) {
       $("#go-watch").html(
-        `<a class='btn btn-purple mx-auto' href='#watch-container'>شاهد ${decodeURIComponent(data.title)}</a>`
+        `<a class='btn btn-purple mx-auto' href='#watch-container'>شاهد ${decodeURIComponent(data.title).replace('الحلقة' ,'شاهد الحلقة')}</a>`
       );
       // Get the element with ID 'watch-container'
       const watchContainer = $("<div id='watch-container'></div>");
