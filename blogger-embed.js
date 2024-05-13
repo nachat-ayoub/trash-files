7899b626bf8e232b4329d32af07e92f85136dcc3
Nachat Ayoub <73206390+nachat-ayoub@users.noreply.github.com>
2024-05-13 23:26:08 +0100

    Update blogger-embed.js

--- blogger-embed.js
+++ blogger-embed.js
@@ -60,10 +60,7 @@ $(document).ready(function () {
       // Append the watchContainer after the article element
       $("article").after(watchContainer);
 
-      // If the element exists, set its innerHTML to the specified iframe code
-      if (watchContainer.length) {
-        watchContainer.attr("id", "watchContainer");
-        watchContainer.html(`
+      watchContainer.html(`
           <ul class='servers list-unstyled d-flex flex-wrap justify-content-center align-items-center mt-2 mb-1'>
             ${data.s
               .split("|")
@@ -98,7 +95,6 @@ $(document).ready(function () {
               <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg>
             </a>
           </div>`);
-      }
     }
   }
 });
