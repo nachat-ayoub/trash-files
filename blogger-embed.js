6fac54f24f656b1d5d3f09f1bed8f66d6b7ae3a8
Nachat Ayoub <73206390+nachat-ayoub@users.noreply.github.com>
2024-05-14 20:21:25 +0100

    Update blogger-embed.js

--- blogger-embed.js
+++ blogger-embed.js
@@ -73,7 +73,7 @@ $(document).ready(function () {
          }
     
         .btn-purple:hover {
-          opacity: 0.85;
+          opacity: 0.8;
           color:#fff;
         }
         .responsive-fs {
@@ -83,13 +83,13 @@ $(document).ready(function () {
           
           @media (max-width: 768px) {  /* Target screens smaller than 768px (common mobile breakpoint) */
             .responsive-fs {
-              font-size: 0.8rem !important;  /* Adjust font size for mobile */
+              font-size: 0.85rem !important;  /* Adjust font size for mobile */
             }
           }`;
       $('head').append(mobileStyles)
       
       $("#go-watch").html(
-        `<a class='btn btn-purple' style='opacity:.7;' href='#'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30px" style="scale:1.6;"><circle cx="18" cy="12" r="0" fill="currentColor"><animate attributeName="r" begin=".67" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"></animate></circle><circle cx="12" cy="12" r="0" fill="currentColor"><animate attributeName="r" begin=".33" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"></animate></circle><circle cx="6" cy="12" r="0" fill="currentColor"><animate attributeName="r" begin="0" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"></animate></circle></svg></a>`
+        `<a class='btn btn-purple' style='opacity:.6;' href='#'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30px" style="scale:1.6;"><circle cx="18" cy="12" r="0" fill="currentColor"><animate attributeName="r" begin=".67" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"></animate></circle><circle cx="12" cy="12" r="0" fill="currentColor"><animate attributeName="r" begin=".33" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"></animate></circle><circle cx="6" cy="12" r="0" fill="currentColor"><animate attributeName="r" begin="0" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"></animate></circle></svg></a>`
       );
       
     // After 1 second, replace the SVG content with the title
