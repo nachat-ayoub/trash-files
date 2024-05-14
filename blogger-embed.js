575b758b246eae4cf7037ac65402650cefb89982
Nachat Ayoub <73206390+nachat-ayoub@users.noreply.github.com>
2024-05-14 01:24:12 +0100

    Update blogger-embed.js

--- blogger-embed.js
+++ blogger-embed.js
@@ -94,7 +94,10 @@ $(document).ready(function () {
             }'>الحلقة التالية
               <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg>
             </a>
-          </div>`);
+          </div>
+          
+          <div class='btn btn-purple mx-auto mt-1'>${decodeURIComponent(data.title) }</div>
+          `);
     }
   }
 });
