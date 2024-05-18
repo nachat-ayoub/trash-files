7fa4687f85dd99a2828603720797c98dbe76922d
Nachat Ayoub <73206390+nachat-ayoub@users.noreply.github.com>
2024-05-18 18:02:55 +0100

    Update blogger-embed.js

--- blogger-embed.js
+++ blogger-embed.js
@@ -149,7 +149,7 @@ $(document).ready(function () {
     $("#watch").attr("src", src);
   }
 
-  $("#episode-title").text(date.title);
+  $("#episode-title").text(data.title);
   changeIframeSrc(episodes[0].embed);
 
   // Function to populate episodes
